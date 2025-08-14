a355a798d7b2bf9fbff36df44074af7d5c7c494a
Bromelda <139258541+Bromelda@users.noreply.github.com>
2025-08-14 12:17:26 -0700

    Delete obj directory

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Bloodcraft
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "io.zfolmt.Bloodcraft";
-        public const string PLUGIN_NAME = "Bloodcraft";
-        public const string PLUGIN_VERSION = "1.11.14";
-    }
-}
